20301eff4b77c073bf09ab99b48ec839648fa67f
manami young <manayou1125@outlook.jp>
2022-07-18 16:24:49 +0900

    showアクションの追加、詳細確認ページを表示

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -6,4 +6,21 @@ class PostsController < ApplicationController
   def new
     @post = Post.new
   end
+
+  def create
+    @post= Post.new(post_params)
+    if  @post.save
+      redirect_to posts_path, notice: 'コメントを投稿しました！'
+    else
+      render :new
+    end
+  end
+
+  def show
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:content)
+  end
 end
